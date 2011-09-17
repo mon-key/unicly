47bfbced5719dc3c6d861ef77e523125b205a5bc
MON KEY <monkey@sandpframing.com>
2011-09-17 12:52:30 -0400

    Fix `%verify-valid-uuid-subclass' -- bad referecnce to non-existent symbol 'sublcass' should have been 'subclass'

--- unicly-extend.lisp
+++ unicly-extend.lisp
@@ -15,7 +15,7 @@
 ;; we the way we've chosen to interact with the base class UUID class
 ;; `unique-universal-identifier'. Basically we treat the slot values of its
 ;; instances as immutable once instantiated and try to go out of our way to
-;; protect its slots. It may have been wiser to define the base class as a
+;; protect those slots. It may have been wiser to define the base class as a
 ;; structure (which would likely open up other complications) and then move the
 ;; slot values of an instantiated UUID structure into a CLOS thing. 
 ;; The original intent behind defining UUIDs as classes was to:
@@ -23,20 +23,21 @@
 ;; class uuid:uuid that existing 3 party code using the uuid would remain
 ;; familar. If we moved to structure based instance we would likely have to
 ;; allocate both a class object and a structure object for each UUID created
-;; In any event, as it stands were sticking with the existing class interface.
+;; In any event, as it stands we're sticking with the existing class interface.
+;; This said, the current interface as defined below is not hooking into the MOP and everything by hand with attempting to 
 ;;
 ;; :USAGE
 ;;
-;; (defclass indexable-v5-uuid (unicly:unique-universal-identifier)
+;; (defclass indexable-uuid (unicly:unique-universal-identifier)
 ;;  ((bit-vector 
 ;;    :reader bit-vector-of-uuid)
 ;;   (integer-128
 ;;    :reader integer-128-of-uuid)))
 ;;
-;; (def-make-v5-uuid-extended indexed indexable-v5-uuid)
+;; (def-make-uuid-extend-class-fun indexed indexable-uuid)
 ;;
 ;; (defmethod update-instance-for-different-class  ((old unicly:unique-universal-identifier)
-;;                                                  (new uuid-indexable-v5)
+;;                                                  (new indexable-uuid)
 ;;                                                  &key)
 ;;   (with-slots (%uuid_time-low
 ;;                %uuid_time-mid
@@ -55,6 +56,8 @@
 ;;           (slot-value new 'integer-128) (unicly::uuid-bit-vector-to-integer (slot-value new 'bit-vector)))))
 ;;
 ;; (make-v5-uuid-indexable *uuid-namespace-dns* "bubba")
+;; (make-v3-uuid-indexable *uuid-namespace-dns* "bubba")
+;; (make-v4-uuid-indexable)
 ;;
 ;;; ==============================
 ;;
@@ -75,7 +78,7 @@
                  (multiple-value-list
                   (subtypep 'uuid-indexable-v5 'unique-universal-identifier)))
     (error "Arg SUBCLASS not `cl:subtypep' the class `unique-universal-identifier'~% ~
-            got: ~S~% type-of: ~S~%" sublcass (type-of subclass))))
+            got: ~S~% type-of: ~S~%" subclass (type-of subclass))))
 
 (defun %verify-class-slots (class)
   ;; (%verify-class-slots 'unique-universal-identifier)
@@ -195,13 +198,37 @@
      (unicly::def-make-uuid-byte-array-extended ,make-extended-suffix ,extended-class)
      (values)))
 
+
 ;;; ==============================
-;; Alternative form of macro `def-make-v5-uuid-extended' that doesn't so easily
+;; Alternative forms of macro `def-make-v5-uuid-extended',
+;; `def-make-v4-uuid-extended' That are likely faster where we don't need to
 ;; accomodate specializations on `cl:update-instance-for-different-class'
-;; with this version evaluating (def-make-v5-uuid-extended indexed some-indexed-class)
-;; would generate two new functions `%digested-v5-uuid-indexed' and `make-v5-uuid-indexed'
-;; With `make-v5-uuid-indexed' remaining as the preferred interface for
-;; generating v5 UUID instances of the class `some-indexed-class'
+;; e.g. where we know that the subclass arguments don't contain additional
+;; direct-slots which need to be accomodated.
+;;
+
+;; NOTE This is likely a faster but won't handle updating the `cl:slot-value's of arg V4-UUID-CLASS
+;; (defmacro def-make-v4-uuid-extended (make-v4-uuid-suffix v4-uuid-class)
+;;   ;; (macroexpand-1 '(def-make-v4-uuid-extended indexable uuid-indexable-v3))
+;;   ;; (%verify-valid-uuid-subclass v4-uuid-class)
+;;   ;; (%verify-class-slots         v4-uuid-class)
+;;   (let ((v4-fun-name 
+;;          (intern (format nil "MAKE-V4-UUID-~A"
+;;                          (string-trim '(#\SPACE #\- #\:) (string-upcase make-v4-uuid-suffix))))))
+;;     `(defun ,v4-fun-name, ()
+;;        (declare (special unicly::*random-state-uuid*)
+;;                 (optimize (speed 3)))
+;;        (let ((*random-state* (the random-state unicly::*random-state-uuid*)))
+;;          (the ,v4-uuid-class
+;;            (make-instance ',v4-uuid-class
+;;                           :%uuid_time-low (the unicly::uuid-ub32 (random #xFFFFFFFF))
+;;                           :%uuid_time-mid (the unicly::uuid-ub16 (random #xFFFF))
+;;                           :%uuid_time-high-and-version  
+;;                           (the unicly::uuid-ub16 (dpb #b0100 (byte 4 12) (ldb (byte 12 0) (the unicly::uuid-ub16 (random #xFFFF)))))
+;;                           :%uuid_clock-seq-and-reserved
+;;                           (the unicly::uuid-ub8  (dpb #b0010 (byte 2  6) (ldb (byte  8 0) (the unicly::uuid-ub8 (random #xFF)))))
+;;                           :%uuid_clock-seq-low (the unicly::uuid-ub8 (random #xFF))
+;;                           :%uuid_node (the unicly::uuid-ub48 (random #xFFFFFFFFFFFF))))))))
 ;;
 ;; (defmacro def-make-v5-uuid-extended (make-v5-uuid-suffix v5-uuid-class)
 ;;   (%verify-valid-uuid-subclass v5-uuid-class)
@@ -214,22 +241,22 @@
 ;;                          (string-trim '(#\SPACE #\- #\:) (string-upcase make-v5-uuid-suffix))))))
 ;;     ;;
 ;;     `(defun ,<DIGESTED-V5-UUID-FUN-NAME> (v5-digest-byte-array)
-;;        (declare (type uuid-byte-array-20 
+;;        (declare (type unicly::uuid-byte-array-20 
 ;;                       v5-digest-byte-array)
-;;                 (inline %uuid_time-low-request
-;;                         %uuid_time-mid-request
-;;                         %uuid_time-high-and-version-request
-;;                         %uuid_clock-seq-and-reserved-request 
-;;                         %uuid_node-request)
+;;                 (inline unicly::%uuid_time-low-request
+;;                         unicly::%uuid_time-mid-request
+;;                         unicly::%uuid_time-high-and-version-request
+;;                         unicly::%uuid_clock-seq-and-reserved-request 
+;;                         unicly::%uuid_node-request)
 ;;                 (optimize (speed 3)))
 ;;        (the ,V5-UUID-CLASS
 ;;          (make-instance ',V5-UUID-CLASS
-;;                         :%uuid_time-low (%uuid_time-low-request v5-digest-byte-array)
-;;                         :%uuid_time-mid (%uuid_time-mid-request v5-digest-byte-array)
-;;                         :%uuid_time-high-and-version (%uuid_time-high-and-version-request v5-digest-byte-array #x05)
-;;                         :%uuid_clock-seq-and-reserved (%uuid_clock-seq-and-reserved-request v5-digest-byte-array)
-;;                         :%uuid_clock-seq-low (the uuid-ub8 (%uuid_clock-seq-low-request v5-digest-byte-array))
-;;                         :%uuid_node (%uuid_node-request v5-digest-byte-array))))
+;;                         :%uuid_time-low (unicly::%uuid_time-low-request v5-digest-byte-array)
+;;                         :%uuid_time-mid (unicly::%uuid_time-mid-request v5-digest-byte-array)
+;;                         :%uuid_time-high-and-version (unicly::%uuid_time-high-and-version-request v5-digest-byte-array #x05)
+;;                         :%uuid_clock-seq-and-reserved (unicly::%uuid_clock-seq-and-reserved-request v5-digest-byte-array)
+;;                         :%uuid_clock-seq-low (the uuid-ub8 (unicly::%uuid_clock-seq-low-request v5-digest-byte-array))
+;;                         :%uuid_node (unicly::%uuid_node-request v5-digest-byte-array))))
 ;;     ;;
 ;;     `(defun ,V5-FUN-NAME (namespace name)
 ;;        (declare (type string name)
@@ -243,7 +270,6 @@
 ;;             (unicly::uuid-digest-uuid-instance #x05 namespace name)))))))
 ;;
 ;;; ==============================
-
 
 
 ;;; ==============================
